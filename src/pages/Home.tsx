920cd725fe0af6d09383739319711d4cc60e722d
Victor <vcwildm@gmail.com>
2021-06-22 16:38:31 -0300

    create components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import {useHistory} from "react-router-dom";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+import {Button} from "../components/Button";
+
+import {useAuth} from "../hooks/useAuth";
+
+import "../styles/auth.scss";
+
+
+export function Home() {
+  const history = useHistory();
+
+  const {user, signInWithGoogle} = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire dúvidas do seu público em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Pergunta Pro Pai" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
